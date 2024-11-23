08ef03abf29fa6b2d244359d68fd68f23ceb31bf
“Chenming <chenming.zhao@mail.utoronto.ca>
2024-11-23 07:22:21 +0000

    unneeded files deleted paper data part done model script file updated model created

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,37 +1,53 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: This script models product prices based on size, type, and time trends 
+#          using Bayesian hierarchical modeling.
+# Author: Chenming Zhao
+# Date: 21 November 2024
+# Contact: chenming.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: 
+#   - This script requires the `tidyverse`, `rstanarm`, and `arrow` libraries 
+#     for data manipulation and Bayesian modeling.
+#   - Ensure that `loblaws_analysis_data.parquet` is available in the specified path 
+#     "/cloud/project/data/02-analysis_data/".
+#   - The output model will be saved as `price_size_type_model.rds` in the `models` folder.
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+model_loblaws_data <- read_parquet("data/02-analysis_data/loblaws_data.parquet")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+#### Prepare data ####
+
+# Ensure product_size and product_type are factors
+model_loblaws_data <- model_loblaws_data %>%
+  mutate(
+    size = factor(size, levels = c("Small", "Large", "Extra Large")),
+    type = factor(type),
+    date = as.Date(date)
   )
 
+### Model data ####
+
+# Bayesian hierarchical model for product prices
+
+price_model <- stan_glmer(
+  current_price ~ size * type + date + (1 | product_id),
+  data = loblaws_data,
+  family = Gamma(link = "log"),
+  prior = normal(0, 5, autoscale = TRUE),
+  prior_intercept = normal(2, 5, autoscale = TRUE),
+  iter = 1000, 
+  chains = 4,      
+  cores = 4
+)
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  price_model,
+  file = "models/price_model.rds"
 )
-
-
